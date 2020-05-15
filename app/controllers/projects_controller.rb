a28456b695f96633269bee567d2a7b06e2aaa788
Jack Cullen <jpcullen20@gmail.com>
2020-05-15 10:17:13 -0600

    Add feature to add a contestant to a project - finish extension 2

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,13 @@ class ProjectsController  <ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def update
+    project = Project.find(params[:id])
+    ContestantProject.create(
+      contestant_id: params["Contestant ID:"],
+      project_id: project.id)
+    redirect_to "/projects/#{project.id}"
+  end
+
 end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe "project show page" do
     visit "/projects/#{@news_chic.id}"
     fill_in "Contestant ID:", with: @kentaro.id
     click_button "Add Contestant To Project"
-    expect(current_path).to eq('/projects/#{@news_chic.id}')
+    expect(current_path).to eq("/projects/#{@news_chic.id}")
     expect(page).to have_content("Number of Contestants: 3")
 
     visit "/contestants"
@@ -34,10 +34,3 @@ RSpec.describe "project show page" do
     end
   end
 end
-# I see a form to add a contestant to this project
-# When I fill out a field with an existing contestants id
-# And hit "Add Contestant To Project"
-# I'm taken back to the project's show page
-# And I see that the number of contestants has increased by 1
-# And when I visit the contestants index page
-# I see that project listed under that contestant's name
