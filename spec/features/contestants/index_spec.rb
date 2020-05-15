7164d11105bfa28b3cb7af215eb40cfcbb24f4fc
Jack Cullen <jpcullen20@gmail.com>
2020-05-15 09:43:25 -0600

    Add spec for user story 3

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -25,10 +25,3 @@ RSpec.describe "contestant index page" do
     end
   end
 end
-# When I visit the contestants index page ("/contestants")
-# I see a list of names of all the contestants
-# And under each contestants name I see a list of the projects (names) that they've been on
-# (e.g.   Kentaro Kameyama
-#         Projects: Litfit, Rug Tuxedo
-#         Jay McCarroll
-#         Projects: LeatherFeather)
